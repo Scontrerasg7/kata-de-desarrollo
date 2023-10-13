87a64fba6471ef518f2d72b7221f3580662ba13f
SebastianC <sebastian.contreras@btgpactual.com>
2023-10-13 11:49:20 -0500

    Added the examples so I can test them later

--- challenges/challenge_three.py
+++ challenges/challenge_three.py
@@ -6,6 +6,8 @@ def challenge_three():
     possible_change_list = build_possible_change_list(sorted_coins)
 
     min_impossible_change = get_min_int_out_of_list(possible_change_list)
+
+    print(min_impossible_change)
         
 
 ################################################################################
@@ -17,8 +19,7 @@ def get_sorted_coins_list():
 def build_possible_change_list(sorted_coins):
     possible_change_list = []
 
-    return possible_change_list
-        
+    return possible_change_list        
 
 def append_change_to_possible_change(possible_change_list, change):
     if change not in possible_change_list:
@@ -34,9 +35,14 @@ def get_min_int_out_of_list(sorted_list):
 
     return min_int
     
-
 ################################################################################
 
 
 if __name__ == "__main__":
     challenge_three()
+
+
+# [5, 7, 1, 1, 2, 3, 22]
+# [1, 1, 1, 1, 1]
+# [1, 5, 1, 1, 1, 10, 15, 20 ,100]
+# [1, 2, 4]
